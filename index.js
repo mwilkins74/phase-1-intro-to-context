7f27ffe197bce8c483374e7c9dc22bf2b038ed98
Mark <markwilkins86@gmail.com>
2022-05-13 10:24:54 -0400

    commit tree fda58c5abe47cc36609402d1765370d5372e073e

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+const csvDataEmployees = [
+    ["Thor", "Odinsson", "Electrical Engineer", 45],
+    ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
+    ["Natalia", "Romanov", "CEO", 150],
+    ["Darcey", "Lewis", "Intern", 15],
+    ["Jarvis", "Stark", "CIO", 125],
+    ["Anthony", "Stark", "Angel Investor", 300]
+  ]
+
+let createEmployeeRecord = function(row) {
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+let createEmployeeRecords = function(employeeRowData) {
+    return employeeRowData.map(function(row) {
+        return createEmployeeRecords(row)
+
+    })
+}
+
+let createTimeInEvent = function(emlpoyee, dataStamp) {
+    let [date, hour]  = dataStamp.split('')
+
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return employee
+}
